8852bd99179730883d8a251daf8ef8f773fc2dee
Kamal LAHOUIR <Kamal-Lahouir@users.noreply.github.com>
2021-03-04 01:29:41 +0100

    Initialization of the project!
    V0.1 is here. Basics!
    Next step : More forms and more fun !
    
    Co-Authored-By: Adel Dahani <69170574+dahani9091@users.noreply.github.com>

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10;
+    Rigidbody myRB;
+    private void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = input.normalized;
+        Vector3 velocity = direction * speed * Time.deltaTime;
+        transform.position += velocity;
+        
+    }
+}
